9d3d759681127e33c4d6a1cef3c5456cecade9aa
saydnyc <saydnyc@gmail.com>
2025-11-01 05:24:50 +0000

    Added a Sessions so code can get responses from the same email
    Added Global vars

--- Test.py
+++ Test.py
@@ -4,14 +4,23 @@ import string
 
 url = "https://1sec-mail.com/get_messages"
 
+Token = ""
+Data = {}
+Response = {}
+Session = {}
+
+def GenerateSession():
+    Session = requests.Session()
+
 def GenerateToken():
     characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(30))
+    Token = ''.join(random.choice(characters) for _ in range(30))
+    return Token
 
 def GenerateData():
-    token = GenerateToken()
-    return {"_token": token,}
+    Data = {"_token": Token,}
+    return Data
 
-response = requests.post(url, data=GenerateData())
-print(response.status_code)
-print(response.text)
+def GetResponse():
+    Response = Session.post(url, data=Data)
+    return Response
